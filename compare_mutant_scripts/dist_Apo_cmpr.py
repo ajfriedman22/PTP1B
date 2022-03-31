82ba23ced0d3b76cdde09ec7b2c9da1524c83fdb
Anika Friedman <anika.friedman@colorado.edu>
2022-03-31 13:35:19 -0400

    Change file paths

--- compare_mutant_scripts/dist_Apo_cmpr.py
+++ compare_mutant_scripts/dist_Apo_cmpr.py
@@ -5,9 +5,13 @@ from scipy import stats
 from itertools import product
 def load_rmsf(path, pair, WT_data):
     raw = open('../../../' + path + '/analysis/' + pair + '_dist.txt').readlines()
-    r = np.zeros(len(raw))
+    num = int(len(raw)/250 + 1)
+    r = np.zeros(num)
+    n = 0
     for i in range(len(raw)):
-        r[i] = float(raw[i])
+        if i%250 == 0:
+            r[n] = float(raw[i])
+            n += 1
     mean = np.mean(r)
     err = stats.sem(r)
     st, p = stats.ttest_ind(WT_data, r, equal_var = False) #Welch's t-test
@@ -32,15 +36,19 @@ def plot_compare(mean, err, Label, sect, n):
 
 #File paths for all input files
 file_path = ['../Apo_dis', 'L192F/Apo', 'E276F/Apo', 'F280Y/Apo', 'L195F/Apo', 'F196A/Apo', 'V287T/Apo'] #Indices to rank in order of closest activity to WT to Furthest
-pairs = ['81_199']
+pairs = ['81_199', '117_182', '117_217', '192_225', '182_220', '187_269', '178_190', '152_177', '151_191', '152_297', '178_150', '179_191', '182_220', '185_191', '187_269', '189_295', '192_225', '200_282', '200_287', '264_185', '276_292', '280_287']
 label = ['WT', 'L192F', 'E276F', 'F280Y', 'L195F', 'F196A', 'V287T']
 mut = ['L192F', 'E276F', 'F280Y', 'L195F', 'F196A', 'V287T']
 
 #Load WT
 raw = open('../../../' + file_path[0] + '/analysis/' + pairs[0] + '_dist.txt').readlines()
-WT_data = np.zeros(len(raw))
+num = int(len(raw)/250 + 1)
+WT_data = np.zeros(num)
+n = 0
 for i in range(len(raw)):
-    WT_data[i] = float(raw[i])
+    if i%250 == 0:
+        WT_data[n] = float(raw[i])
+        n += 1
 
 #Empty Vector for means
 Dist_mean = np.zeros((len(file_path), len(pairs)))
@@ -54,8 +62,19 @@ p = np.zeros((len(mut), len(pairs)))
 #Load the rest of the data
 for i in range(1, len(file_path)):
     n = i-1
-    Dist_mean[i][0], Dist_err[i][0], p[n][0] = load_rmsf(file_path[i], pairs[0], WT_data)
+    for j in range(len(pairs)):
+        if j >0:
+            raw = open('../../../' + file_path[0] + '/analysis/' + pairs[j] + '_dist.txt').readlines()
+            WT_data = np.zeros(len(raw))
+            for k in range(len(raw)):
+                WT_data[k] = float(raw[k])
+            Dist_mean[0,j] = np.mean(WT_data)
+            Dist_err[0,j] = stats.sem(WT_data)
+
+
+        Dist_mean[i][j], Dist_err[i][j], p[n][j] = load_rmsf(file_path[i], pairs[j], WT_data)
 
 #Plot compare
-plot_compare(Dist_mean, Dist_err, label, pairs, 0)
+for i in range(len(pairs)):
+    plot_compare(Dist_mean, Dist_err, label, pairs, i)
 
