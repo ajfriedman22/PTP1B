c2eccffa3f0b65324d80a047a4d0190eae7211d2
Anika Friedman <anika.friedman@colorado.edu>
2022-03-31 13:40:49 -0400

    Fix file paths

--- compare_scripts/lig_helix_indv_cmpr.py
+++ compare_scripts/lig_helix_indv_cmpr.py
@@ -1,55 +1,36 @@
+#Determine the difference in helical interactions b/w AD and BBR
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy import stats
 
+def load_file(file_dir):
+    num_inter = []
+    for i in open('../../../' + file_dir + '/all_iter_frac.txt', 'r').readlines():
+        num_inter.append(float(i.split(' ')[1]))
+    bond = 0
+    num_frame = max(num_inter)
+    all_inter = np.zeros(len(num_inter))
+    for i in num_inter:
+        all_inter[bond] = (i / num_frame) *100
+        bond += 1
+
+    return all_inter
 #Bond names
 group_l = [300]
 group_p  = [186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298]
 
 #Empty array for each set of percentages
 num_bonds = len(group_p)
-all_inter = np.zeros([8, num_bonds])
 
 #Open input files or contacts over time
-bond = 0
-for i in open('../../../1sug_dis_AD/config11/all_iter_frac.txt', 'r').readlines():
-    all_inter[0][bond] = (float(i.split(' ')[1]) / 7376) *100
-    bond += 1
+dir_list = ['1sug_dis_AD/analysis/config11', '1sug_dis_AD/analysis/config_alt', '1sug_dis_AD/analysis/config_alt2', 'AD_dis/analysis/config11', 'mutate/WT/AD/analysis', 'BBR_a7/analysis', 'BBR_1sug/analysis', 'BBR_dis/analysis/config9', 'BBR_1sug_dis/analysis/config7', 'mutate/WT/BBR/analysis'] 
 
-bond = 0
-for i in open('../../../1sug_AD_dis_alt/run_1/all_iter_frac.txt', 'r').readlines():
-    all_inter[1][bond] = (float(i.split(' ')[1]) / 3334) * 100
-    bond += 1
+#Empty array for each set of percentages
+num_bonds = len(group_p)
+all_inter = np.zeros([len(dir_list), num_bonds])
 
-bond = 0
-for i in open('../../../1sug_AD_dis_alt/run_2/all_iter_frac.txt', 'r').readlines():
-    all_inter[2][bond] = (float(i.split(' ')[1]) / 11251) * 100
-    bond += 1
-
-bond = 0
-for i in open('../../../AD_dis/config11/all_iter_frac.txt', 'r').readlines():
-    all_inter[3][bond] = (float(i.split(' ')[1]) / 6872) * 100
-    bond += 1
-
-bond = 0
-for i in open('../../../BBR_a7/all_iter_frac.txt', 'r').readlines():
-    all_inter[4][bond] = (float(i.split(' ')[1]) / 4600) * 100
-    bond += 1
-
-bond = 0
-for i in open('../../../1sug_BBR/all_iter_frac.txt', 'r').readlines():
-    all_inter[5][bond] = (float(i.split(' ')[1]) / 3688) * 100
-    bond += 1
-
-bond = 0
-for i in open('../../../BBR_dis/config9/all_iter_frac.txt', 'r').readlines():
-    all_inter[6][bond] = (float(i.split(' ')[1]) / 4801) * 100
-    bond += 1
-
-bond = 0
-for i in open('../../../1sug_dis_BBR/config7/all_iter_frac.txt', 'r').readlines():
-    all_inter[7][bond] = (float(i.split(' ')[1]) / 3626) * 100
-    bond += 1
+for i in range(len(dir_list)):
+    all_inter[i,:] = load_file(dir_list[i])
 
 #Output File for significant bonds
 output_AD_freq = open('Sign_inter_AD_freq.txt', 'w')
@@ -63,10 +44,10 @@ mean_inter = np.zeros([4, num_bonds])
 sem_inter = np.zeros([4, num_bonds])
 
 for n in range(num_bonds):
-    AD_open = [all_inter[0][n], all_inter[1][n], all_inter[2][n]]
+    AD_open = [all_inter[0][n], all_inter[1][n], all_inter[2][n], all_inter[4][n]]
     AD_close = all_inter[3][n]
-    BBR_open = [all_inter[4][n], all_inter[5][n]]
-    BBR_close = [all_inter[6][n], all_inter[7][n]]
+    BBR_open = [all_inter[5][n], all_inter[6][n], all_inter[9][n]]
+    BBR_close = [all_inter[7][n], all_inter[8][n]]
 
     mean_inter[0][n] = np.mean(AD_open) #AD Open
     mean_inter[1][n] = AD_close #AD Closed
@@ -97,7 +78,7 @@ for n in range(num_bonds):
    
     diff = abs(mean_inter[0][n] - mean_inter[3][n])
 
-    if p1 < 0.05 and diff > 3:
+    if p1 < 0.05:
         output_cmpr.write(str(group_p[n]) + ' + AD/BBR:\n'+ 'AD: ' + str(mean_inter[0][n]) + ' +/- ' + str(sem_inter[0][n]) + '\nBBR: ' + str(mean_inter[3][n]) + ' +/- ' + str(sem_inter[3][n]) + '\np = ' + str(p1) + '\n')
         #Bar plot
         num = [1, 2]
