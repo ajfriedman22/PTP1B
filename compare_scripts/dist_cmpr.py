16133be7e274bf562d9f5c5dc8fef48d46c25e13
Anika Friedman <anika.friedman@colorado.edu>
2022-05-13 21:02:28 -0400

    Update file input system and documentation

--- compare_scripts/dist_cmpr.py
+++ compare_scripts/dist_cmpr.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from itertools import product
+
+#Import custom modules
+sys.path.insert(1, '/ocean/projects/cts160011p/afriedma/PTP1B/util/')
+import plot
+
 def plot_func(dist, err, label, p, p1, p2):
     num = [5, 10, 15, 20]
     Method = ['Apo Open', 'Apo Closed', 'AD', 'BBR']
@@ -13,51 +18,10 @@ def plot_func(dist, err, label, p, p1, p2):
     ax1.bar(num, dist, color = ['black', 'black', 'darkblue', 'darkred', 'blue', 'red'], width=4.5)
     plt.errorbar(num, dist, yerr= err, fmt='o', color='black')
     plt.xticks(num, Method, fontsize=14)
-    if p < 0.05 and p > 0.01:
-        x1, x2 = 5, 15 #Columns for Apo and AD
-        y, h, col = (1.1*dist[[0, 2]].max()), 1, 'b'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-    if p < 0.01 and p > 0.001:
-        x1, x2 = 5, 15 #Columns for Apo and AD
-        y, h, col = (1.1*dist[[0, 2]].max()), 1, 'b'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-    if p < 0.001:
-        x1, x2 = 5, 15 #Columns for Apo and AD
-        y, h, col = (1.1*dist[[0, 2]].max()), 1, 'b'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-    if p1 < 0.05 and p1 > 0.01:
-        x1, x2 = 5, 20 #Columns for Apo and BBR
-        y, h, col = (1.1*dist[[0, 3]].max()), 1, 'r'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-    if p1 < 0.01 and p1 > 0.001:
-        x1, x2 = 5, 20 #Columns for Apo and BBR
-        y, h, col = (1.1*dist[[0, 3]].max()), 1, 'r'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-    if p1 < 0.001:
-        x1, x2 = 5, 20 #Columns for Apo and BBR
-        y, h, col = (1.1*dist[[0, 3]].max()), 1, 'r'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-    if p2 < 0.05 and p2 > 0.01:
-        x1, x2 = 5, 10 #Columns for Apo and AD
-        y, h, col = (1.1*dist[[0, 1]].max()), 1, 'k'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-    if p2 < 0.01 and p2 > 0.001:
-        x1, x2 = 5, 10 #Columns for Apo and AD
-        y, h, col = (1.1*dist[[0, 1]].max()), 1, 'k'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-    if p2 < 0.001:
-        x1, x2 = 5, 10 #Columns for Apo and AD
-        y, h, col = (1.1*dist[[0, 1]].max()), 1, 'k'
-        plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-        plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
+
+    plot.error_bar(5, 15, dist[0], dist[2], p, 1, 'b'):
+    plot.error_bar(5, 20, dist[0], dist[3], p1, 1, 'r'):
+    plot.error_bar(5, 10, dist[0], dist[1], p2, 1, 'k'):
 
     fig.savefig(label + '_inter.png')
     plt.close(fig)
@@ -68,6 +32,10 @@ label = ['L11_a3', 'L11_a7', 'WPD_L11', 'WPD_a3_top', 'WPD_a3', 'a3_a7_top', 'a3
 eq_time = [5, 5, 50, 5, 75, 5, 5, 5, 70, 5, 60, 30]
 tot_time = [200, 200, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300]
 
+#Load file path
+file_path = open('Input_path/dist_file_path.txt', 'r').readlines()
+
+#Load distance for all pairs
 for i in range(len(pairs)):
     d_AD, d_BBR = [],[]
     d_Apo_open, d_Apo_close = [],[]
@@ -75,31 +43,32 @@ for i in range(len(pairs)):
     d_BBR_a7, d_AD_dis11, d_AD_alt, d_AD_alt2 = [],[],[],[]
 
     #Input Data for a3 and a6 residue dist
-    for j in open('../../../mutate/WT/AD/analysis/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[0] + pairs[i] + 'dist.txt').readlines():
         d_WT_AD.append(float(j))
-    for j in open('../../../mutate/WT/BBR/analysis/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[1] + pairs[i] + 'dist.txt').readlines():
         d_WT_BBR.append(float(j))
-    for j in open('../../../rebuild_a7/analysis/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[2] + pairs[i] + 'dist.txt').readlines():
         d_a7.append(float(j))
-    for j in open('../../../rebuild_a7_high/config9/analysis/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[3] + pairs[i] + 'dist.txt').readlines():
         d_dis9.append(float(j))
-    for j in open('../../../rebuild_a7_high/config11/analysis/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[4] + pairs[i] + 'dist.txt').readlines():
         d_dis11.append(float(j))
-    for j in open('../../../Apo_1SUG/analysis/1sug/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[5] + pairs[i] + 'dist.txt').readlines():
         d_1sug.append(float(j))
-    for j in open('../../../Apo_1SUG/analysis/1sug2/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[6] + pairs[i] + 'dist.txt').readlines():
         d_1sug2.append(float(j))
-    for j in open('../../../Apo_1SUG/analysis/1sug3/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[7] + pairs[i] + 'dist.txt').readlines():
         d_1sug3.append(float(j))
-    for j in open('../../../BBR_a7/analysis/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[8] + pairs[i] + 'dist.txt').readlines():
         d_BBR_a7.append(float(j))
-    for j in open('../../../1sug_dis_AD/analysis/config11/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[9] + pairs[i] + 'dist.txt').readlines():
         d_AD_dis11.append(float(j))
-    for j in open('../../../1sug_dis_AD/analysis/config_alt/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[10] + pairs[i] + 'dist.txt').readlines():
         d_AD_alt.append(float(j))
-    for j in open('../../../1sug_dis_AD/analysis/config_alt2/' + pairs[i] + 'dist.txt').readlines():
+    for j in open(file_path[11] + pairs[i] + 'dist.txt').readlines():
         d_AD_alt2.append(float(j))
     
+    #Seperate into categories
     d_Apo_open = np.concatenate((d_dis9, d_dis11))
     d_Apo_close = np.concatenate((d_1sug, d_1sug2, d_1sug3))
     d_AD = np.concatenate((d_AD_dis11, d_AD_alt, d_AD_alt2))
