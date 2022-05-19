c192bfcdc7fb3732e70052f3457fc09d2f7a85bb
Anika Friedman <anika.friedman@colorado.edu>
2022-05-19 10:45:09 -0400

    Incorporate modules and limit AD bound to loc1 and loc2

--- compare_scripts/lig_helix_inter_cmpr.py
+++ compare_scripts/lig_helix_inter_cmpr.py
@@ -1,77 +1,63 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy import stats
+import sys
+
+#Import custom modules
+sys.path.insert(1,'/ocean/projects/cts160011p/afriedma/code/PTP1B/util')
+import plot
+
+def load_file(dir_path, inters):
+    a3, a4, a5, a6, a7 = [],[],[],[],[]
+    for i in range(len(dir_path)):
+        for j in range(len(inters)):
+            data = open('../../../' + dir_path[i] + '/' + inters[j] + '_inter.txt').readlines()
+            if j == 0:
+                for k in data:
+                    a3.append(float(k))
+            if j == 1:
+                for k in data:
+                    a4.append(float(k))
+            if j == 2:
+                for k in data:
+                    a5.append(float(k))
+            if j == 3:
+                for k in data:
+                    a6.append(float(k))
+            if j == 4:
+                for k in data:
+                    a7.append(float(k))
+    return a3, a4, a5, a6, a7
+
+def plot_helix_inter(helix, err, hel_num, p):
+    num = [1, 2]
+    label = ['AD', 'BBR']
+
+    fig = plt.figure()
+    plt.title(r'Interactions with $\alpha$' + str(hel_num) + ' Helix', fontsize = 14)
+    plt.ylabel('Mean Simultaneous Interactions', fontsize = 12)
+    plt.bar(num, helix, color = ['blue', 'purple'], width=0.8)
+    plt.errorbar(num, helix, yerr=err, fmt='o', color='black')
+    plt.xticks(num, label, fontsize=12)
+    plot.error_bar(1, 2, helix[0], helix[1], p, 0.5, 'k')
+    fig.savefig('a' + str(hel_num) + '_helix_inter_cmpr.png')
+    plt.close(fig)
 
 #List of all directory paths for each group
 dir_path_AD_crys = ['mutate/WT/AD/analysis', '1sug_dis_AD/analysis/config11']
 dir_path_AD_alt = ['1sug_dis_AD/analysis/config_alt', '1sug_dis_AD/analysis/config_alt2']
-dir_path_BBR = ['mutate/WT/BBR/analysis', 'BBR_a7/analysis']
+dir_path_AD_alt2 = ['AD_rebuild_a7/analysis', 'AD/analysis', '1sug_AD/analysis']
+dir_path_BBR = ['mutate/WT/BBR/analysis', 'BBR_a7/analysis', 'BBR_dis/analysis/config11']
 
 #List interactions of interest
 inters = ['a3', 'a4', 'a5', 'a6', 'a7']
 
-#Set all empty arrays
-a3_AD_crys, a4_AD_crys, a5_AD_crys, a6_AD_crys, a7_AD_crys = [],[],[],[],[]
-a3_AD_alt, a4_AD_alt, a5_AD_alt, a6_AD_alt, a7_AD_alt = [],[],[],[],[]
-a3_BBR, a4_BBR, a5_BBR, a6_BBR, a7_BBR = [],[],[],[],[]
-
 #Load all data
-for i in range(len(dir_path_AD_crys)):
-    for j in range(len(inters)):
-        data = open('../../../' + dir_path_AD_crys[i] + '/' + inters[j] + '_inter.txt').readlines()
-        if j == 0:
-            for k in data:
-                a3_AD_crys.append(float(k))
-        if j == 1:
-            for k in data:
-                a4_AD_crys.append(float(k))
-        if j == 2:
-            for k in data:
-                a5_AD_crys.append(float(k))
-        if j == 3:
-            for k in data:
-                a6_AD_crys.append(float(k))
-        if j == 4:
-            for k in data:
-                a7_AD_crys.append(float(k))
+a3_AD_crys, a4_AD_crys, a5_AD_crys, a6_AD_crys, a7_AD_crys = load_file(dir_path_AD_crys, inters)
+a3_AD_alt, a4_AD_alt, a5_AD_alt, a6_AD_alt, a7_AD_alt = load_file(dir_path_AD_alt, inters)
+#a3_AD_alt2, a4_AD_alt2, a5_AD_alt2, a6_AD_alt2, a7_AD_alt2 = load_file(dir_path_AD_alt2, inters)
+a3_BBR, a4_BBR, a5_BBR, a6_BBR, a7_BBR = load_file(dir_path_BBR, inters)
 
-for i in range(len(dir_path_AD_alt)):
-    for j in range(len(inters)):
-        data = open('../../../' + dir_path_AD_alt[i] + '/' + inters[j] + '_inter.txt').readlines()
-        if j == 0:
-            for k in data:
-                a3_AD_alt.append(float(k))
-        if j == 1:
-            for k in data:
-                a4_AD_alt.append(float(k))
-        if j == 2:
-            for k in data:
-                a5_AD_alt.append(float(k))
-        if j == 3:
-            for k in data:
-                a6_AD_alt.append(float(k))
-        if j == 4:
-            for k in data:
-                a7_AD_alt.append(float(k))
-
-for i in range(len(dir_path_BBR)):
-    for j in range(len(inters)):
-        data = open('../../../' + dir_path_BBR[i] + '/' + inters[j] + '_inter.txt').readlines()
-        if j == 0:
-            for k in data:
-                a3_BBR.append(float(k))
-        if j == 1:
-            for k in data:
-                a4_BBR.append(float(k))
-        if j == 2:
-            for k in data:
-                a5_BBR.append(float(k))
-        if j == 3:
-            for k in data:
-                a6_BBR.append(float(k))
-        if j == 4:
-            for k in data:
-                a7_BBR.append(float(k))
 
 #Determine the mean and sem for each helix interactions
 a3_AD = np.append(a3_AD_alt, a3_AD_crys)
@@ -79,6 +65,8 @@ a4_AD = np.append(a4_AD_alt, a4_AD_crys)
 a5_AD = np.append(a5_AD_alt, a5_AD_crys)
 a6_AD = np.append(a6_AD_alt, a6_AD_crys)
 a7_AD = np.append(a7_AD_alt, a7_AD_crys)
+
+#a3_AD_all = np.append(a3_AD, a3_AD_alt2)
 
 helix_a3 = np.array([np.mean(a3_AD), np.mean(a3_BBR)])
 helix_a4 = np.array([np.mean(a4_AD), np.mean(a4_BBR)])
@@ -130,127 +118,22 @@ output.write('p-value b/w AD and BBR: ' + str(p_a7) + '\n')
 num = [1, 2]
 label = ['AD', 'BBR']
 #Plot Bar graph comparing averages for a3 helix
-fig = plt.figure()
-plt.title('Interactions with a3 Helix')    
-plt.ylabel('Mean Number of Interactions')
-plt.bar(num, helix_a3[:], color = ['green', 'blue'], width=0.8)
-plt.errorbar(num, helix_a3[:], yerr=err_a3[:], fmt='o', color='black')
-plt.xticks(num, label, fontsize=8)
-if p_a3 < 0.05 and p_a3 > 0.01:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a3.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-if p_a3 < 0.01 and p_a3 > 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a3.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-if p_a3 < 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a3.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-fig.savefig('a3_helix_inter_cmpr.png')
-plt.close(fig)
+plot_helix_inter(helix_a3, err_a3, 3, p_a3)
 
 #Plot Bar graph comparing averages for a4 helix
-fig = plt.figure()
-plt.title('Interactions with a4 Helix')    
-plt.ylabel('Mean Number of Interactions')
-plt.bar(num, helix_a4[:], color = ['green', 'blue'], width=0.8)
-plt.errorbar(num, helix_a4[:], yerr=err_a4[:], fmt='o', color='black')
-plt.xticks(num, label, fontsize=8)
-if p_a4 < 0.05 and p_a4 > 0.01:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a4.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-if p_a4 < 0.01 and p_a4 > 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a4.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-if p_a4 < 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a4.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-fig.savefig('a4_helix_inter_cmpr.png')
-plt.close(fig)
+plot_helix_inter(helix_a4, err_a4, 4, p_a4)
 
 #Plot Bar graph comparing averages for a5 helix
-fig = plt.figure()
-plt.title('Interactions with a5 Helix')    
-plt.ylabel('Mean Number of Interactions')
-plt.bar(num, helix_a5[:], color = ['green', 'blue'], width=0.8)
-plt.errorbar(num, helix_a5[:], yerr=err_a5[:], fmt='o', color='black')
-plt.xticks(num, label, fontsize=8)
-if p_a5 < 0.05 and p_a5 > 0.01:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a5.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-if p_a5 < 0.01 and p_a5 > 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a5.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-if p_a5 < 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a5.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-fig.savefig('a5_helix_inter_cmpr.png')
-plt.close(fig)
+plot_helix_inter(helix_a5, err_a5, 5, p_a5)
 
 #Plot Bar graph comparing averages for a6 helix
-fig = plt.figure()
-plt.title('Interactions with a6 Helix')    
-plt.ylabel('Mean Number of Interactions')
-plt.bar(num, helix_a6[:], color = ['green', 'blue'], width=0.8)
-plt.errorbar(num, helix_a6[:], yerr=err_a6[:], fmt='o', color='black')
-plt.xticks(num, label, fontsize=8)
-if p_a6 < 0.05 and p_a6 > 0.01:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a6.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-if p_a6 < 0.01 and p_a6 > 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a6.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-if p_a6 < 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a6.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-fig.savefig('a6_helix_inter_cmpr.png')
-plt.close(fig)
+plot_helix_inter(helix_a6, err_a6, 6, p_a6)
 
 #Plot Bar graph comparing averages for a7 helix
-fig = plt.figure()
-plt.title('Interactions with a7 Helix')    
-plt.ylabel('Mean Number of Interactions')
-plt.bar(num, helix_a7[:], color = ['green', 'blue'], width=0.8)
-plt.errorbar(num, helix_a7[:], yerr=err_a7[:], fmt='o', color='black')
-plt.xticks(num, label, fontsize=8)
-if p_a7 < 0.05 and p_a7 > 0.01:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a7.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "*" , ha='center', va='bottom', color=col)
-if p_a7 < 0.01 and p_a7 > 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a7.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "**" , ha='center', va='bottom', color=col)
-if p_a7 < 0.001:
-    x1, x2 = 1, 2 #Columns for AD crystal and BBR
-    y, h, col = (1.1*helix_a7.max()) + 2, 2, 'k'
-    plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-    plt.text((x1+x2)*0.5, y+h, "***" , ha='center', va='bottom', color=col)
-fig.savefig('a7_helix_inter_cmpr.png')
-plt.close(fig)
+plot_helix_inter(helix_a7, err_a7, 7, p_a7)
+print((helix_a3[0]-helix_a3[1])/helix_a3[1])
+print((helix_a7[0]-helix_a7[1])/helix_a7[1])
+
+plot.plot_gen_box(a3_AD, a3_BBR, 'AD', 'BBR', p_a3, 12, 'Ligand', 'Mean Simultaneous Interactison', '', 'a3_helix_box_cmpr.png', 8, 8)
+#plot.plot_gen_box(a3_AD_all, a3_BBR, 'AD', 'BBR', p_a3, 12, 'Ligand', 'Mean Simultaneous Interactison', '', 'a3_helix_box_all_cmpr.png', 8, 8)
 
